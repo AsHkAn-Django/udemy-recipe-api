d06b6b785807f3c476f7e3da63e55e76b8583f24
Ashkan <ashkan_ahrari@yahoo.com>
2025-06-08 13:14:16 +0300

    configure docker compose and checks to wait for db

--- app/app/test.py
+++ app/app/test.py
@@ -8,15 +8,14 @@ from . import calc
 
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
-    
+
     def test_add_number(self):
         """Test adding numbers together."""
         res = calc.add(5, 6)
-        
+
         self.assertEqual(res, 11)
-    
+
     def test_subtract_number(self):
         """Test subtracting nubmbers together."""
         res = calc.subtract(6, 5)
         self.assertEqual(res, 1)
-        
